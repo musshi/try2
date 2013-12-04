d8045f3e0bd766b811e6101869ccb039d3374e98
Musshi <musshi@rubify.com>
2013-12-04 15:34:14 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Try2::Application.config.secret_token = '96a9dfc555032b44cb9368e6e813d5d4ae71cb6750961c39b15e4783f85f5bf2e06c1b59535d692b095f54bae4c83e10bddbd7a84148f2f861223fba966db2ae'
---
+++ db/migrate/20131204061911_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.boolean :completed
+      t.references :list
+
+      t.timestamps
+    end
+    add_index :tasks, :list_id
+  end
+end
